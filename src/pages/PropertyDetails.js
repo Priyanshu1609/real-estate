9e17b68f1c8b67f8e459fa9d8908c1385a3850e5
xRAIDENx <priyanshupanda.ctp@gmail.com>
2022-11-13 12:04:52 +0530

    ui changes

--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -58,7 +58,7 @@ const PropertyDetails = () => {
         <div className='flex-1 w-full mb-8 bg-white border border-gray-300 rounded-lg px-6 py-8'>
           <div className='flex items-center gap-x-4 mb-8'>
             <div className='w-20 h-20 p-1 border border-gray-300 rounded-full'>
-              <img src={property.agent.image}></img>
+              <img src={property.agent.image} alt="property"></img>
             </div>
             <div>
               <div className='font-bold text-lg'>{property.agent.name}</div>
@@ -72,16 +72,19 @@ const PropertyDetails = () => {
               className='border border-gray-300 focus:border-red-400 rounded w-full px-4 h-14 text-sm outline-none'
               type='text'
               placeholder='Name*'
+              required
             />
             <input
               className='border border-gray-300 focus:border-red-400 rounded w-full px-4 h-14 text-sm outline-none'
-              type='text'
+              type='email'
               placeholder='Email*'
+              required
             />
             <input
               className='border border-gray-300 focus:border-red-400 rounded w-full px-4 h-14 text-sm outline-none'
-              type='text'
+              type='tel'
               placeholder='Phone*'
+              required
             />
             <textarea
               className='border border-gray-300 focus:border-red-400 rounded w-full p-4 h-36 text-sm text-gray-400 outline-none resize-none'
